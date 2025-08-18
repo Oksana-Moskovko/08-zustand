fa31d89c03ccfa49557f2ad80b912d2e53f4eb97
Oksana <130862204+Oksana-Moskovko@users.noreply.github.com>
2025-08-18 14:48:24 +0300

    Update not-found.tsx

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -8,19 +8,6 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "404 - Page Not Found",
   description: "The page you are looking for does not exist.",
-  openGraph: {
-    title: "404 - Page not found",
-    description: "the page you are looking for does not exist",
-    images: [
-      {
-        url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
-        width: 1200,
-        height: 630,
-        alt: "404 - Page not found",
-      },
-    ],
-    type: "article",
-  },
 };
 
 const NotFound = () => {
