395f3d2a798a6f8d83cc4deff94bdb11905bbb3c
Oksana <omoskovko98@gmail.com>
2025-08-18 14:44:21 +0300

    dekete url

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,12 +6,11 @@ import { useEffect } from "react";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "404 - Page not found",
-  description: "the page you are looking for does not exist",
+  title: "404 - Page Not Found",
+  description: "The page you are looking for does not exist.",
   openGraph: {
     title: "404 - Page not found",
     description: "the page you are looking for does not exist",
-    url: `https://notehub.com/404-Page-not-found`,
     images: [
       {
         url: "https://ac.goit.global/fullstack/react/notehub-og-meta.jpg",
